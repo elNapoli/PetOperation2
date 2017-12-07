b16ad9e5939f046a11aa7d485902cc30d5d58db2
Baldomero Napoli <baldomero.napoli@gmail.com>
2017-12-06 22:00:11 -0300

    se modularizo el toolbar

--- app/src/main/java/com/example/elnapoli/petoperation/activities/AboutActivity.java
+++ app/src/main/java/com/example/elnapoli/petoperation/activities/AboutActivity.java
@@ -2,6 +2,8 @@ package com.example.elnapoli.petoperation.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
 import com.example.elnapoli.petoperation.R;
 
 public class AboutActivity extends AppCompatActivity {
@@ -10,5 +12,8 @@ public class AboutActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 }
--- app/src/main/java/com/example/elnapoli/petoperation/activities/ContactActivity.java
+++ app/src/main/java/com/example/elnapoli/petoperation/activities/ContactActivity.java
@@ -3,6 +3,7 @@ package com.example.elnapoli.petoperation.activities;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -15,7 +16,9 @@ public class ContactActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact);
-
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         Button btnSendEmail = (Button) findViewById(R.id.btnSendEmail);
 
         btnSendEmail.setOnClickListener(new View.OnClickListener() {
